9dade75d766534837f7094887856203ffb63c4d4
UraFIZ <kratser88@gmail.com>
2021-02-24 14:09:36 +0200

    setup eslint

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import Recipes from "src/components/Recipes";
-import sword from "src/images/swc-sword.png";
-import swordSvg from "src/images/sword.svg";
+import React, { FC } from 'react';
+import Recipes from 'src/components/Recipes';
+import sword from 'src/images/swc-sword.png';
+import swordSvg from 'src/images/sword.svg';
 
 import 'src/styles/index.scss';
 
-const App = () => {
-  return (
-    <>
-      <section className="hero"></section>
-      <main>
-        <section>
-          <h1>Oh Herro, React.</h1>
-          <img src={sword} alt="sword" width="250" />
-          <img src={swordSvg} alt="sword" width="250" />
-          <Recipes />
-        </section>
-      </main>
-    </>
-  );
-};
+const App: FC = () => (
+  <>
+    <section className="hero" />
+    <main>
+      <section>
+        <h1>Oh Herro, React.</h1>
+        <img src={sword} alt="sword" width="250" />
+        <img src={swordSvg} alt="sword" width="250" />
+        <Recipes />
+      </section>
+    </main>
+  </>
+);
 
 export default App;
--- src/components/Recipes.tsx
+++ src/components/Recipes.tsx
@@ -1,12 +1,10 @@
 // Importing "react" is still required when using methods from it
-import React from "react";
+import React, { FC } from 'react';
 
-const Recipes = () => {
-  return (
-    <div>
-      <h3>Current Recipe:</h3>
-    </div>
-  );
-};
+const Recipes: FC = () => (
+  <div>
+    <h3>Current Recipe:</h3>
+  </div>
+);
 
 export default Recipes;
